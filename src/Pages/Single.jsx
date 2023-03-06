3a5dfa108d6725e057d7b55f37e10b396427db54
amStevee <stevenanongo2@gmail.com>
2023-03-06 01:55:51 +0100

    usericon

--- src/Pages/Single.jsx
+++ src/Pages/Single.jsx
@@ -56,63 +56,59 @@ const Single = () => {
         <Loading />
       ) : (
         <>
-          {post.username ? (
-            <SinglePage>
-              <div className="content">
-                <img
-                  src={`https://day-dream-server.onrender.com/${post.image}`}
-                  alt="content"
-                />
-                <div className="user">
-                  {post.user_image ? (
-                    <div className="fauser">
-                      <FontAwesomeIcon
-                        color="gray"
-                        fontSize={"2rem"}
-                        icon={faUser}
-                      />
-                    </div>
-                  ) : (
-                    <img
-                      src={`https://day-dream-server.onrender.com/${post?.user_image}`}
-                      alt="user"
+          <SinglePage>
+            <div className="content">
+              <img
+                src={`https://day-dream-server.onrender.com/${post.image}`}
+                alt="content"
+              />
+              <div className="user">
+                {post.user_image ? (
+                  <div className="fauser">
+                    <FontAwesomeIcon
+                      color="gray"
+                      fontSize={"2rem"}
+                      icon={faUser}
                     />
-                  )}
-                  <div className="info">
-                    <span>{post.username}</span>
-                    <p>posted {moment(post.created_at).fromNow()}</p>
                   </div>
-                  {currentUser.username === post.username && (
-                    <div className="edit">
-                      <Link to={`/posts/write?edit=${post.id}`} state={post}>
-                        <FontAwesomeIcon
-                          color="green"
-                          fontSize={"1.5rem"}
-                          icon={faEdit}
-                        />
-                      </Link>
-                      <i onClick={handleDelete}>
-                        <FontAwesomeIcon
-                          color="red"
-                          fontSize={"1.5rem"}
-                          icon={faTrash}
-                        />
-                      </i>
-                    </div>
-                  )}
+                ) : (
+                  <img
+                    src={`https://day-dream-server.onrender.com/${post?.user_image}`}
+                    alt="user"
+                  />
+                )}
+                <div className="info">
+                  <span>{post.username}</span>
+                  <p>posted {moment(post.created_at).fromNow()}</p>
                 </div>
-                <h1>{post.title}</h1>
-                <ReactQuill
-                  value={post.description}
-                  readOnly={true}
-                  theme={"bubble"}
-                />
+                {currentUser.username === post.username && (
+                  <div className="edit">
+                    <Link to={`/posts/write?edit=${post.id}`} state={post}>
+                      <FontAwesomeIcon
+                        color="green"
+                        fontSize={"1.5rem"}
+                        icon={faEdit}
+                      />
+                    </Link>
+                    <i onClick={handleDelete}>
+                      <FontAwesomeIcon
+                        color="red"
+                        fontSize={"1.5rem"}
+                        icon={faTrash}
+                      />
+                    </i>
+                  </div>
+                )}
               </div>
-              <Aside category={post.category} />
-            </SinglePage>
-          ) : (
-            navigate("/login")
-          )}
+              <h1>{post.title}</h1>
+              <ReactQuill
+                value={post.description}
+                readOnly={true}
+                theme={"bubble"}
+              />
+            </div>
+            <Aside category={post.category} />
+          </SinglePage>
         </>
       )}
       <Footer />
