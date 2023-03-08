39176a61b14a4e15af2e5ccf878ef06022e1b798
amStevee <stevenanongo2@gmail.com>
2023-03-08 13:46:07 +0100

    user account

--- src/styles/UserAccount.styled.js
+++ src/styles/UserAccount.styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const User = styled.div`
-display: flex;
-flex-direction: column;
-
-`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 300px;
+`;
 
 export default User;
