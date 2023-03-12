499c8d840e2f1011a24286cdc7b525096ff183fc
amStevee <stevenanongo2@gmail.com>
2023-03-12 04:45:01 +0100

    image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -71,6 +71,7 @@ const Home = () => {
                         src={`${imageUrl}${post.image}`}
                         height={"200px"}
                         width={"100%"}
+                        PlaceholderSrc={"../../public/stockphoto.jpg"}
                       />
                     </div>
 
