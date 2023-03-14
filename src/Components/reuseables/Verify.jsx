36269905391b80c78e58f15ce8f17fff341eff83
amStevee <stevenanongo2@gmail.com>
2023-03-14 16:27:13 +0100

    verify delt.

--- src/Components/reuseables/Verify.jsx
+++ src/Components/reuseables/Verify.jsx
@@ -1,4 +1,3 @@
-import { Button } from "bootstrap";
 import VerifyComponent from "../../styles/Verify.styled";
 
 export default function Verify({
@@ -12,8 +11,8 @@ export default function Verify({
       <div className={activedel ? "rmv" : "contain"}>
         <h3>Are you sure you want to delete this {desc} </h3>
         <div className="btn">
-          <Button onClick={handleDelete}>Yes</Button>
-          <Button onClick={() => setActivedel(false)}>No</Button>
+          <button onClick={handleDelete}>Yes</button>
+          <button onClick={() => setActivedel(false)}>No</button>
         </div>
       </div>
     </VerifyComponent>
