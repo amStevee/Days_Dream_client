b4c59b05696893ccca95edf0b5643e1ccc571d95
amStevee <stevenanongo2@gmail.com>
2023-04-11 23:32:09 +0100

    updated footer

--- src/styles/Footer.styled.js
+++ src/styles/Footer.styled.js
@@ -12,6 +12,10 @@ const FooterComponent = styled.footer`
   gap: 1rem;
 
   .header {
+    h2 {
+      font-size: larger;
+    }
+
     .hcontentent {
       display: flex;
       justify-content: space-between;
@@ -44,35 +48,6 @@ const FooterComponent = styled.footer`
       justify-content: space-between;
       align-items: center;
       gap: 10px;
-
-      .instagram {
-        background: #f09433;
-        background: -moz-linear-gradient(
-          45deg,
-          #f09433 0%,
-          #e6683c 25%,
-          #dc2743 50%,
-          #cc2366 75%,
-          #bc1888 100%
-        );
-        background: -webkit-linear-gradient(
-          45deg,
-          #f09433 0%,
-          #e6683c 25%,
-          #dc2743 50%,
-          #cc2366 75%,
-          #bc1888 100%
-        );
-        background: linear-gradient(
-          45deg,
-          #f09433 0%,
-          #e6683c 25%,
-          #dc2743 50%,
-          #cc2366 75%,
-          #bc1888 100%
-        );
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
-      }
     }
   }
 `;
