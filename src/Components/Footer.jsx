a5340e02985a5ed15b9f01d9093a57c2257a7270
amStevee <stevenanongo2@gmail.com>
2023-04-12 00:10:47 +0100

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,58 +18,63 @@ const Footer = () => {
   const year = date.getFullYear();
   const { currentUser } = useContext(AuthContext);
   return (
-    <FooterComponent>
-      <h2>Days Dream Blog</h2>
-      <div className="header">
-        <div className="hcontentent">
-          <Link to={currentUser?.username ? "/posts/write" : "/login"}>
-            <button>Write for us</button>
-          </Link>
-          <Link to={"/about"}>
-            <u>About Us</u>
-          </Link>
-          <Link to={"/about"}>
-            <u>Terms of use</u>
-          </Link>
+    <>
+      <FooterComponent>
+        <h2>Days Dream Blog</h2>
+        <div className="header">
+          <div className="hcontentent">
+            <Link to={currentUser?.username ? "/posts/write" : "/login"}>
+              <button>Write for us</button>
+            </Link>
+            <Link to={"/about"}>
+              <u>About Us</u>
+            </Link>
+            <Link to={"/about"}>
+              <u>Terms of use</u>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h4>Developed by:</h4>
-        <a href="https://twitter.com/am_stevenanongo" style={{ color: "#fff" }}>
-          Steven Anongo
-        </a>
-      </div>
-
-      <div className="social">
-        <h4>Socials:</h4>
-        <div className="icons">
-          <a href="https://daysdreamhub@gmail.com">
-            <FontAwesomeIcon fontSize={"2rem"} icon={faEnvelope} />
-            daysdreamhub@gmail.com
-          </a>
-          <a href="https://twitter.com/am_stevenanongo">
-            <FontAwesomeIcon fontSize={"2rem"} icon={faTwitterSquare} />
-            Twitter
-          </a>
-          <a href="https://instagram.com/am_stevee">
-            <FontAwesomeIcon fontSize={"2rem"} icon={faInstagramSquare} />
-            Instagram
-          </a>
-
-          <a href="https://youtube.com/daysdreamhub">
-            <FontAwesomeIcon fontSize={"2rem"} icon={faYoutubeSquare} />
-            @daysdreamhub
-          </a>
-          <a href="https://telegram.com/daysdreamhub">
-            <FontAwesomeIcon fontSize={"2rem"} icon={faTelegram} />
-            @Daysdreamhub
+        <div>
+          <h4>Developed by:</h4>
+          <a
+            href="https://twitter.com/am_stevenanongo"
+            style={{ color: "#fff" }}
+          >
+            Steven Anongo
           </a>
         </div>
-      </div>
 
-      <div className="copyright">&copy; {year} Days Dream Blog</div>
-    </FooterComponent>
+        <div className="social">
+          <h4>Socials:</h4>
+          <div className="icons">
+            <a href="https://daysdreamhub@gmail.com">
+              <FontAwesomeIcon fontSize={"2rem"} icon={faEnvelope} />
+              daysdreamhub@gmail.com
+            </a>
+            <a href="https://twitter.com/am_stevenanongo">
+              <FontAwesomeIcon fontSize={"2rem"} icon={faTwitterSquare} />
+              Twitter
+            </a>
+            <a href="https://instagram.com/am_stevee">
+              <FontAwesomeIcon fontSize={"2rem"} icon={faInstagramSquare} />
+              Instagram
+            </a>
+
+            <a href="https://youtube.com/daysdreamhub">
+              <FontAwesomeIcon fontSize={"2rem"} icon={faYoutubeSquare} />
+              @daysdreamhub
+            </a>
+            <a href="https://telegram.com/daysdreamhub">
+              <FontAwesomeIcon fontSize={"2rem"} icon={faTelegram} />
+              @Daysdreamhub
+            </a>
+          </div>
+        </div>
+
+      </FooterComponent>
+        <div className="copyright">&copy; {year} Days Dream Blog</div>
+    </>
   );
 };
 
